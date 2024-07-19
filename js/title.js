e5102018d1c06aaed33075981ba1c52432d2f7b1
Canmi <CanmiCN@gmail.com>
2024-07-19 21:34:29 +0800

    Site updated: 2024-07-19 21:34:29

--- js/title.js
+++ js/title.js
@@ -1,11 +1,12 @@
 var OriginTitile = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function() {
+document.addEventListener(‘visibilitychange’, function() {
     if (document.hidden) {
-        document.title = '(つェ⊂)我藏好了哦~';
+        document.title = ‘(つェ⊂)我藏好了哦~ ‘;
         clearTimeout(titleTime);
-    } else {
-        document.title = '(*´∇｀*)被你发现了w';
+    }
+    else {
+        document.title = ‘(*´∇｀*) 被你发现啦~ ‘;
         titleTime = setTimeout(function() {
             document.title = OriginTitile;
         }, 2000);
